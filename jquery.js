8f5495edfc4f74aa4eae04370e15d6256288e962
darkenergy1856 <aryanchandra7360@gmail.com>
2022-08-12 21:05:55 +0530

    Some More Changes and Time Pass

--- jquery.js
+++ jquery.js
@@ -1,18 +1,28 @@
 function letsChange() {
-    alert('Its Work');
+    $("table").addClass("tableClass");
+    $("html").addClass("htmlClass");
+    $("#main").addClass("divClass");
+    $("#heading").addClass("heading");
+    $("#fee").addClass("fee");
+    $("th").addClass("th");
+    $("td").addClass("td");
+    $("#criteria").addClass("element criteria");
+    $("#eligibility").addClass("element eligibility");
+    $("#future").addClass("future");
+    $("#futureElement").addClass("futureClass");
+    $("button").hide();
 };
 
 $(function () {
 
-    $("table").addClass("tableClass");
-    $("html").addClass("htmlClass");
-    $("#main").addClass("divClass")
-
-    // Everything below this Show be in this function only!!!
-    // Make class for every css.
-    $("button").css("font-size", "20px");
-    $("button").hover(function () {
-        $(this).css("font-size", "30px")
-    }, function () { $(this).css("font-size", "20px") }
-    );
+    // $("table").addClass("tableClass");
+    // $("html").addClass("htmlClass");
+    // $("#main").addClass("divClass");
+    // $("#heading").addClass("heading");
+    // $("#fee").addClass("fee");
+    // $("th").addClass("th");
+    // $("td").addClass("td");
+    // $("#criteria").addClass("element criteria");
+    // $("#eligibility").addClass("element eligibility");
+    // $("#future").addClass("futureClass")
 })
