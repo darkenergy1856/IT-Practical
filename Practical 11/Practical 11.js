9e76325f6a4af8a2cb4415a702bd172ddaca3c5e
darkenergy1856 <aryanchandra7360@gmail.com>
2022-08-20 20:17:52 +0530

    Practical 11 Complete 😁😁😁

--- Practical 11/Practical 11.js
+++ Practical 11/Practical 11.js
@@ -1,6 +1,6 @@
 var signInForm = true;
-var signUp = '<label for="firstName">First Name : </label><br />    <input type="text" id="firstName" /><br />    <label for="lastName">Last Name : </label><br />    <input type="text" id="lastName" /><br />    <label for="emailID">Email ID : </label><br /><input type="email" id="emailID" autocomplete="username"/><br /><label for="password">Password : </label><br /><input type="password" id="password" autocomplete="new-password"/><br /><label for="confirmPassword">Confirm Password : </label><br /><input type="password" id="confirmPassword" autocomplete="new-password"/><br /><button type="button">Create Account</button><button type="button" onclick="changeForm()">Login</button>';
-var signIn = '<label for="emailID">Email ID : </label><br /><input type="email" id="emailID" autocomplete="username"/><br /><label for="password">Password : </label><br /><input type="password" id="password" autocomplete="current-password"/><br /><div class="buttonGroup"><button type="button" onclick="changeForm()">Sign Up</button><button type="button" onClick="login()">Login</button></div>'
+var signUp = '<label for="firstName">First Name : </label><br />    <input type="text" id="firstName" /><br />    <label for="lastName">Last Name : </label><br />    <input type="text" id="lastName" /><br />    <label for="emailID">Email ID : </label><br /><input type="email" id="emailID" autocomplete="username"/><br /><label for="password">Password : </label><br /><input type="password" id="password" autocomplete="new-password"/><br /><label for="confirmPassword">Confirm Password : </label><br /><input type="password" id="confirmPassword" autocomplete="new-password"/><br /><button type="button" onClick="createAccount()">Create Account</button><button type="button" onclick="changeForm()">Login</button>';
+var signIn = '<label for="emailID">Email ID : </label><br /><input type="email" id="emailID" autocomplete="username"/><br /><label for="password">Password : </label><br /><input type="password" id="password" autocomplete="current-password"/><br /><div class="buttonGroup"><button type="button" onClick="login()">Login</button><button type="button" onclick="changeForm()">Sign Up</button></div>'
 
 
 $(function () {
@@ -12,27 +12,54 @@ const url = 'http://localhost:10000/';
 HTTP.open("get", url + 'welcome');
 HTTP.send();
 
-// HTTP.onreadystatechange = () => {
-//     if (HTTP.readyState == 4 && HTTP.status < 400) {
-//         console.log(HTTP.response);
-//     }
-// }
-
 function changeForm() {
-    if(signInForm){
+    if (signInForm) {
         document.getElementById('changeState').innerHTML = signUp;
         signInForm = false;
-    }else{
+    } else {
         document.getElementById('changeState').innerHTML = signIn;
         signInForm = true;
     }
 }
 
-function login(){
+function login() {
     var password = document.getElementById("password").value;
     var emailId = document.getElementById("emailID").value;
-    console.log(password , emailId);
-    HTTP.open("get" , url+'authenticate/'+emailId+'/'+password , false);
+    console.log(password, emailId);
+    HTTP.open("GET", url + 'authenticate/' + emailId + '/' + password, false);
     HTTP.send();
     console.log(HTTP.responseText);
+    if (HTTP.responseText === 'true') {
+        welcomeUser();
+    }
+    else {
+        confirm("Credential Invalid !! Try Again..");
+    }
+}
+
+function createAccount() {
+    var password = document.getElementById("password").value;
+    var emailId = document.getElementById("emailID").value;
+    var confirmPassword = document.getElementById("confirmPassword").value;
+    var name = document.getElementById("firstName").value + " " + document.getElementById("lastName").value;
+    if (confirmPassword === password) {
+        HTTP.open("POST" , url+'new/' + name +'/' +emailId +'/' + password , false );
+        console.log( url+'new/' + name +'/' +emailId +'/' + password);
+        HTTP.send();
+        console.log(HTTP.responseText);
+        if(HTTP.responseText === 'true'){
+            confirm("Account Created Successfully");
+            location.href = "index.html";
+        }else{
+            confirm("Email ID already in use . Try Again with different Email ID.")
+        }
+    }
+    else {
+        confirm("Password didn't match !! Try again");
+    }
+}
+
+function welcomeUser() {
+    var welcome = '<p style = "font-size: xx-large">Welcome User !!</p>';
+    document.getElementById('changeState').innerHTML = welcome;
 }
